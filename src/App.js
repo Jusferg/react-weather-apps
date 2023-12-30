3dfbaa714887117be315c2eeb7dfb9d034287a24
Tia <90842451+Jusferg@users.noreply.github.com>
2023-12-30 12:35:32 -0500

    added API integration with axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
-      <footer>
+        <Weather defaultCity="New York"/>
+          <footer>
         This weather app was coded by Tia Ferguson and is {" "}
-      <a href="https://github.com/Jusferg/react-weather-apps" target="_blank">open-sourced on GitHub</a>
-      </footer>
+            <a href="https://github.com/Jusferg/react-weather-apps" 
+            target="_blank">open-sourced on GitHub</a>
+          </footer>
       </div>
     </div>
   );
